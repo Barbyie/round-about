90a691a3ff198a5b1a026fab708cfd768cc72c1d
Thiago Barbieri <thiago.barbieri2002@gmail.com>
2023-10-19 19:58:36 -0300

    Fixed issue where the round_about would fail because the backup_frequency wasn't an integer.

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ name_map = input("Map Folder Name:\n")
 map_path_folder = input("Path that your world folder is located:\n")
 backup_folder_name = input("Backup Folder Name:\n")
 backup_path_folder = input("Path that the backup folder will be created:\n")
-backup_frequency = input("Frequency that you want to take backups (in seconds):\n")
+backup_frequency = int(input("Frequency that you want to take backups (in seconds):\n"))
 
 with open('variables.pk1', 'wb') as file:
     
