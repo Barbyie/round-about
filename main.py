360cd12b216acc3d18bb67092ea2792721197b87
Thiago Barbieri <thiago.barbieri2002@gmail.com>
2023-11-12 14:33:27 -0300

    Add Time and Compress class

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ import datetime
 
 class Backup:
 
-    def __init__(self, name, time, origin, destination):
+    def __init__(self, name, time, origin=os.path.dirname(__file__)+"/world", destination=os.path.dirname(__file__)+"/roundabout"):
         self.name = name
         self.time = time
         self.origin = origin
         self.destination = destination
     
     def trigger(self):        
-            pass
+            command = subprocess.run()
 
 class Time:
 
-    def __init__(self):
+    def __str__(self):
         current_time = datetime.datetime.now()
         formatted_time = current_time.strftime("%y-%m-%d-%I-%M-%S-%p")
         return formatted_time
@@ -26,8 +26,9 @@ class Time:
 
 class Compress:
 
-    def __init__(self, file):
-        self.file = file
+    def __str__(self):
+        compress = f"tar -czvf {Backup.destination}/{Backup.name}-{Time()} {Backup.origin}"
+        return compress
 
 class Map:
 
