e9e0dc27e81e777875995772df0425041533bb99
Thiago Barbieri Catarina <thiago.barbieri2002@gmail.com>
2023-11-15 14:17:30 -0300

    Add backup completion and failed message

--- main.py
+++ main.py
@@ -3,42 +3,33 @@ import subprocess
 import datetime
 
 
-
-
 class Backup:
 
-    def __init__(self, name, time, origin=os.path.dirname(__file__)+"/world", destination=os.path.dirname(__file__)+"/roundabout"):
+    def __init__(self, name, origin=os.path.dirname(__file__) + "/world",
+                 destination=os.path.dirname(__file__) + "/thiago"):
         self.name = name
-        self.time = time
         self.origin = origin
         self.destination = destination
-    
-    def trigger(self):        
-            command = subprocess.run()
+
+    def trigger(self):
+                time_instance = Time()
+                try:
+                    compress = f"tar -czvf {self.destination}/{self.name}-{time_instance.formatted_time} {self.origin}"
+                    compress_exec = subprocess.run(compress, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                               text=True, check=True)
+                    print(f"Backup created successfully at {time_instance.formatted_time}")
+                except subprocess.CalledProcessError as error_message:
+
+                    print("Backup creation failed with error:\n", error_message.stderr)
+
+
 
 class Time:
 
-    def __str__(self):
+    def __init__(self):
         current_time = datetime.datetime.now()
-        formatted_time = current_time.strftime("%y-%m-%d-%I-%M-%S-%p")
-        return formatted_time
-        #current_time will get the current time and formatted_time will format the time in YY-MM-DD-HH-MM-SS-AM/PM.
+        self.formatted_time = current_time.strftime("%y-%m-%d-%I-%M-%S-%p")
 
-class Compress:
+call_backup = Backup(name="thiago")
+call_backup.trigger()
 
-    def __str__(self):
-        compress = f"tar -czvf {Backup.destination}/{Backup.name}-{Time()} {Backup.origin}"
-        return compress
-
-class Map:
-
-    def __init__(self, name, path):
-        self.name = name
-        self.path = path
-
-
-class Log:
-
-    def __init__(self, message, time):
-        self.message = message
-        self.time = time
